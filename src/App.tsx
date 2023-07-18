b27049182cd19fc22238f2e2f202a440fafea82f
Karrius <Ecetris@gmail.com>
2023-07-18 03:55:44 +0200

    styles, app component, redux provier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import logo from "./logo.svg"
-import { Counter } from "./features/counter/Counter"
-import "./App.css"
+import ActionButtons from './featuers/listControls/components/actionButtons';
+import ColorFilter from './featuers/listControls/components/colorFilter';
+import StatusFilter from './featuers/listControls/components/statusFilter';
+import RemianingTasks from './featuers/listControls/components/remianingTasks';
+import TaskInput from './featuers/tasks/components/taskInput';
+import TaskList from './featuers/tasks/components/taskList';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <TaskInput />
+      <TaskList />
+      <div className="controls-wrapper">
+        <ActionButtons />
+        <RemianingTasks />
+        <StatusFilter />
+        <ColorFilter />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
